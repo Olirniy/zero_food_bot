28ea398601145a3d7caa0ab264d3483539980f16
Olirniy <divvushonn@yandex.ru>
2025-05-25 19:34:45 +0300

    Add README

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ setup(
     name="zero_food_bot",
     version="0.1",
     packages=find_packages(),
+    extras_require={
+        'dev': [
+            'pytest>=7.0',
+            'pytest-cov>=4.0',
+        ],
+    }
 )
